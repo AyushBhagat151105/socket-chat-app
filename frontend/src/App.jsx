53810e03eb3a741acbcec2770478bfb4480e595c
Ayush_Bhagat <103896902+AyushBhagat151105@users.noreply.github.com>
2025-06-30 22:39:34 +0530

    Refactor frontend structure: update HTML doctype, add react-router for navigation, and implement basic page components (Home, Login, SignUp, Settings, Profile). Introduce Navbar component and update package dependencies.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,24 @@
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router";
+import HomePage from "./page/HomePage";
+import SignUpPage from "./page/SignUpPage";
+import LoginPage from "./page/LoginPage";
+import SettingsPage from "./page/SettingsPage";
+import ProfilePage from "./page/ProfilePage";
+
 function App() {
   return (
-    <>
-      <div className="text-amber-300">Hii there</div>
-    </>
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
   );
 }
 
