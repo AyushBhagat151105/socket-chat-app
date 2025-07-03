8dcc580ebec1b468927c620598b75bdb75b4bdcc
Ayush_Bhagat <103896902+AyushBhagat151105@users.noreply.github.com>
2025-07-03 19:11:05 +0530

    Add ChatHeader and MessageInput components

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useChatStore } from "../store/useChat.store";
+import MessageSkeleton from "./skeletons/MessageSkeleton";
+import MessageInput from "./MessageInput";
 
 function ChatContainer() {
+  const { messages, getMessages, isMessagesLoading, selectedUser } =
+    useChatStore();
+
+  useEffect(() => {
+    getMessages(selectedUser._id);
+  }, [selectedUser._id, getMessages]);
+  if (isMessagesLoading) return <MessageSkeleton />;
+
   return (
-    <div>ChatContainer</div>
-  )
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+
+      <p>Messages...</p>
+
+      <MessageInput />
+    </div>
+  );
 }
 
-export default ChatContainer
+export default ChatContainer;
